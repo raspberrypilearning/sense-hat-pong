4b93a607eb6ba6a60aae86ae11fded5ddd435d9e
majamanojlovic <49232422+majamanojlovic@users.noreply.github.com>
2023-02-06 12:20:07 +0000

    New translations pong2.py (French)

--- fr-FR/resources/pong2.py
+++ fr-FR/resources/pong2.py
@@ -7,51 +7,51 @@ sense = SenseHat()
 sense.clear(0, 0, 0)
 
 y = 4
-ball_position=[6, 3]
-ball_speed=[-1, -1]
+balle_position=[6, 3]
+balle_vitesse=[-1, -1]
 
-def draw_bat():
+def dessine_raquette():
     sense.set_pixel(0, y, 255, 255, 255)
     sense.set_pixel(0, y+1, 255, 255, 255)
     sense.set_pixel(0, y-1, 255, 255, 255)
         
-def ball_play():
-    sense.set_pixel(ball_position[0], ball_position[1], 0, 0, 0)
+def balle_joue():
+    sense.set_pixel(balle_position[0], balle_position[1], 0, 0, 0)
 
-    ball_position[0] += ball_speed[0]
-    ball_position[1] += ball_speed[1]
+    balle_position[0] += balle_vitesse[0]
+    balle_position[1] += balle_vitesse[1]
 
-    if ball_position[1] == 0 or ball_position[1] == 7:
-        ball_speed[1] = -ball_speed[1]
-    if ball_position[0] == 7:
-        ball_speed[0] = -ball_speed[0]
-    if ball_position[0] == 1 and y-1 <= ball_position[1] <= y+1:
-        ball_speed[0] = -ball_speed[0]
-    if ball_position[0] == 0:
+    if balle_position[1] == 0 or balle_position[1] == 7:
+        balle_vitesse[1] = -balle_vitesse[1]
+    if balle_position[0] == 7:
+        balle_vitesse[0] = -balle_vitesse[0]
+    if balle_position[0] == 1 and y-1 <= balle_position[1] <= y+1:
+        balle_vitesse[0] = -balle_vitesse[0]
+    if balle_position[0] == 0:
         return False
 
-    sense.set_pixel(ball_position[0], ball_position[1], 0, 0, 255)
+    sense.set_pixel(balle_position[0], balle_position[1], 0, 0, 255)
     return True
 
-def move_up(event):
+def deplace_haut(event):
     global y
     if y > 1 and event.action=='pressed':
         y -= 1
     print(event)
 
-def move_down(event):
+def deplace_bas(event):
     global y
     if y < 6 and event.action=='pressed':
         y += 1
     print(event)
 
 
-sense.stick.direction_up = move_up
-sense.stick.direction_down = move_down
+sense.stick.direction_up = deplace_haut
+sense.stick.direction_down = deplace_bas
 
-while ball_play():
-    draw_bat()
+while balle_joue():
+    dessine_raquette()
     sleep(0.25)
     sense.clear(0, 0, 0)
     
-sense.show_message("You Lose", text_colour=(255, 0, 0))
+sense.show_message("Tu as perdu", text_colour=(255, 0, 0))
