6686cd51a442ffefc22a07bb6689e528c403b4b8
majamanojlovic <49232422+majamanojlovic@users.noreply.github.com>
2023-02-06 12:18:48 +0000

    New translations build.py (French)

--- fr-FR/resources/build.py
+++ fr-FR/resources/build.py
@@ -4,45 +4,45 @@ from sense_emu import SenseHat
 sense = SenseHat()
 
 y = 4
-ball_position = [3, 3]
-ball_velocity = [1, 1]
+balle_position = [3, 3]
+balle_vitesse = [1, 1]
 
-def draw_bat():
+def dessine_raquette():
     sense.set_pixel(0, y, 255, 255, 255)
     sense.set_pixel(0, y+1, 255, 255, 255)
     sense.set_pixel(0, y-1, 255, 255, 255)
         
-def move_up(event):
+def deplace_haut(event):
     global y
     if y > 1 and event.action=='pressed':
         y -= 1
     print(event)
 
-def move_down(event):
+def deplace_bas(event):
     global y
     if y < 6 and event.action=='pressed':
         y += 1
     print(event)
 
-def draw_ball():
-    sense.set_pixel(ball_position[0], ball_position[1], 0, 0, 255)
-    ball_position[0] += ball_velocity[0]
-    ball_position[1] += ball_velocity[1]
-    if ball_position[0] == 7:
-        ball_velocity[0] = -ball_velocity[0]
-    if ball_position[1] == 0 or ball_position[1] == 7:
-        ball_velocity[1] = -ball_velocity[1]
-    if ball_position[0] == 0:
-        sense.show_message("You Lose", text_colour=(255, 0, 0))
+def dessine_balle():
+    sense.set_pixel(balle_position[0], balle_position[1], 0, 0, 255)
+    balle_position[0] += balle_vitesse[0]
+    balle_position[1] += balle_vitesse[1]
+    if balle_position[0] == 7:
+        balle_vitesse[0] = -balle_vitesse[0]
+    if balle_position[1] == 0 or balle_position[1] == 7:
+        balle_vitesse[1] = -balle_vitesse[1]
+    if balle_position[0] == 0:
+        sense.show_message("Tu as perdu", text_colour=(255, 0, 0))
         quit()
-    if ball_position[0] == 1 and y - 1 <= ball_position[1] <= y+1:
-        ball_velocity[0] = -ball_velocity[0]
+    if balle_position[0] == 1 and y - 1 <= balle_position[1] <= y+1:
+        balle_vitesse[0] = -balle_vitesse[0]
         
-sense.stick.direction_up = move_up
-sense.stick.direction_down = move_down
+sense.stick.direction_up = deplace_haut
+sense.stick.direction_down = deplace_bas
 
 while True:
     sense.clear(0, 0, 0)
-    draw_bat()
-    draw_ball()
+    dessine_raquette()
+    dessine_balle()
     sleep(0.25)
